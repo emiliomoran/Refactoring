1b7bb39f7ebc60a26b61e798005b4dabedb59caf
emilio97 <emiliojmp9@gmail.com>
2018-08-06 22:14:14 -0500

    Long Method

---
+++ ExamplesRefactoring/src/dataClumps/bad/Pago.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataClumps.bad;
+
+/**
+ *
+ * @author EmilioMoran
+ */
+public class Pago {
+    public void SubmitCreditCardOrder(String firstName, String lastName, String zipcode, String streetAddress1, String streetAddress2, String city, String state, String country, String phoneNumber, String creditCardNumber, int expirationMonth, int expirationYear, float saleAmount){
+        // … submit order
+    }
+}
---
+++ ExamplesRefactoring/src/dataClumps/good/Pago.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataClumps.good;
+
+/**
+ *
+ * @author Moises
+ */
+public class Pago {
+    public void SubmitCreditCardOrder(ContactInformation customerInfo, CreditCard card, float saleAmount) {
+        // … submit order
+    }
+}
