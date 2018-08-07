55d33548bf6db6596d8ecd11280695b16c622fe7
Cliente CONTROL-PC <CltControl@GGIECLB3WRK076.espol.edu.ec>
2018-08-07 14:57:47 -0500

    Data Class

---
+++ ExamplesRefactoring/src/dataClass/bad/Shape.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataClass.bad;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Shape {
+    private int lados;
+    private int angulos;
+    private int vertices;
+
+    public int getLados() {
+        return lados;
+    }
+
+    public void setLados(int lados) {
+        this.lados = lados;
+    }
+
+    public int getAngulos() {
+        return angulos;
+    }
+
+    public void setAngulos(int angulos) {
+        this.angulos = angulos;
+    }
+
+    public int getVertices() {
+        return vertices;
+    }
+
+    public void setVertices(int vertices) {
+        this.vertices = vertices;
+    }
+    
+    
+}
---
+++ ExamplesRefactoring/src/dataClass/good/Shape.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataClass.good;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Shape {
+    private int lados;
+    private int angulos;
+    private int vertices;
+
+    public void getLados() {
+        System.out.println(lados);
+    }    
+
+    public void getAngulos() {
+        System.out.println(angulos);
+    }
+
+    public void getVertices() {
+        System.out.println(vertices);
+    }    
+    
+    public void addLado(int num){
+        
+    }
+    
+    public void removeLado(int num){
+        
+    }
+    
+    //Metodos de add y remove para cada atributo
+}
